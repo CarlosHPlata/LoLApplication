948459c19c24abff5c51ca88285fe656602cee62
kingskull619 <chewbaca_central@hotmail.com>
2015-09-17 18:09:47 -0500

    creating class silvercoding

---
+++ app/src/main/java/com/kingskull/lolapplication/api/restfull/connections/SILVERCODING.java
@@ -0,0 +1,11 @@
+package com.kingskull.lolapplication.api.restfull.connections;
+
+/**
+ * Created by cherrera on 9/17/2015.
+ */
+public final class SILVERCODING {
+
+    public static final String END_POINT_KEY = "";
+    public static final String PROJECT = "personal";
+
+}
